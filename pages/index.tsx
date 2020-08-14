99bb8f7986c4f284c2705eefe88842ba5cd8683b
Mattias Hising <hising@gmail.com>
2020-08-14 11:27:39 +0200

    fancier imports for components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {Button} from "../lib/components/Button";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Button } from "@components/Button";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next JS opinonated setup</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Next JS opinonated setup</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-          <p>Next JS opinonated setup</p>
+            <main className={styles.main}>
+                <p>Next JS opinonated setup</p>
 
-          <Button>Holy Smoke Button</Button>
-          <Button type={"alert"}>Holy Smoke Button</Button>
-          <Button type={"success"}>Holy Smoke Button</Button>
-      </main>
-    </div>
-  )
+                <Button>Holy Smoke Button</Button>
+                <Button type={"alert"}>Holy Smoke Button</Button>
+                <Button type={"success"}>Holy Smoke Button</Button>
+            </main>
+        </div>
+    );
 }
